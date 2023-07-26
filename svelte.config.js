3c57aa2ea8732101800cb6ca73f09d3802c40657
saya <saya.wow@outlook.com>
2023-07-26 20:51:28 +0800

    static-adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from "@sveltejs/kit/vite"
 import { mdsvex, escapeSvelte } from "mdsvex"
 import shiki from "shiki"
@@ -25,6 +25,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        //  Vercel have zero-config support
 		adapter: adapter(),
 		alias: {
 			'styled-system': './styled-system/*'
